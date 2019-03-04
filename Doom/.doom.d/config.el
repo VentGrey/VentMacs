167b21e3224a615746eb489d85d9760c7469d52f
VentGrey <omarpurataf@gmail.com>
2019-03-03 23:18:12 -0600

    Added doom emacs

---
+++ Doom/.doom.d/config.el
@@ -0,0 +1,5 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+(setq doom-font (font-spec :family "Fira Mono" :size 14))
+(global-set-key [f3] 'neotree-toggle)
